acab2cfff4df0b72df1ded64e11828b19388df5e
Giorgio Marinelli <giorgio@marinel.li>
2025-05-22 16:02:34 +0200

    Add some code

---
+++ a/A.hs
@@ -0,0 +1,6 @@
+module A where
+
+import B qualified
+
+a :: B.SomeType
+a = B.someValue
---
+++ b/B.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE DerivingStrategies #-}
+
+module B where
+
+newtype SomeType = SomeValue Int
+  deriving newtype (Num)
+
+someValue :: SomeType
+someValue = 123
